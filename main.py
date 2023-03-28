e4391c0af25b142ab884150467dcc48660f7c456
Luis Pelegrina Gutiérrez <luispelegrinagutierrez@eduroam056143.ugr.es>
2023-03-28 14:28:49 +0200

    Plot FFT Gauss

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ t0=3
 s0=0.5
 gauss = np.exp(-(t-t0)**2/(2*s0**2))
 
+plt.figure()
 plt.plot(t, gauss)
+plt.grid()
+
+plt.figure()
+
+freq = np.fft.fftfreq(len(gauss))
+gaussF = np.fft.fft(gauss)
+plt.plot(freq,np.abs(gaussF))
 plt.grid()
 plt.show()
